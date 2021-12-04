f6815b0e0028762828f50c75e08badf22a482ddc
louisdelperugia <louis.delperugia@unil.ch>
2021-12-04 18:06:49 +0100

    Update R functions.R

--- test/test/R functions.R
+++ test/test/R functions.R
@@ -1,4 +1,4 @@
-poids <- readxl::read_xlsx("/Users/ROUGE/Desktop/VenusLab - QMS poids.xlsx")
+poids <- readxl::read_xlsx("~/NESTLE/PodSPC - Documents/Data/VenusLab - QMS poids.xlsx")
 poids <- poids[!is.na(poids$Prelevement),]
 poids <- poids[!is.na(poids$Cible),]
 poids$Poids1 <- as.double(poids$Poids1)
@@ -31,53 +31,67 @@ request_CL <- function(request, A2 = 0.483, d2 = 5.534) {
   UCL = median(request_SPC$median) + A2*Rbar
   LCL = median(request_SPC$median) - A2*Rbar
   
-  #USL = request_SPC$Cible[1] + request_SPC$Cible[1]*0.01 #Ask to Joao : What value should be set for the gap around the target
-  #LSL = request_SPC$Cible[1] - request_SPC$Cible[1]*0.01 #Ask to Joao : What value should be set for the gap around the target
-  #Cp = (USL - LSL)/(6*Rbar/d2)
-  #Pp = (USL - LSL)/(6*median(request_SPC$sd))
-  #Cpu <- (USL - mean(request_SPC$median))/(3*Rbar/d2)
-  #Cpl <- (mean(request_SPC$median) - LSL)/3*(3*Rbar/d2)
-  #Cpk <- min(Cpu, Cpl)
+  
+  z <- request_SPC$median > UCL | request_SPC$median < LCL
+  out_control_perc <- sum(z)/length(request_SPC)
+  
+  
+  USL = request_SPC$Cible[1] + request_SPC$Cible[1]*0.015 #Ask to Joao : What value should be set for the gap around the target
+  LSL = request_SPC$Cible[1] - request_SPC$Cible[1]*0.015 #Ask to Joao : What value should be set for the gap around the target
+  Cp = (USL - LSL)/(6*Rbar/d2)
+  Cpu <- (USL - mean(request_SPC$median))/(3*Rbar/d2)
+  Cpl <- (mean(request_SPC$median) - LSL)/(3*Rbar/d2)
+  Cpk <- min(Cpu, Cpl)
   
 
   graph <- request_SPC %>%
     ggplot2::ggplot() +
-    ggplot2::geom_histogram(aes(x = request_SPC$median, fill = request_SPC$median > UCL | request_SPC$median < LCL ), bins = 100) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = mean(request_SPC$median)), color = "blue", linetype = 3) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = UCL), color = "blue", linetype = 3) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = LCL), color = "blue", linetype = 3) +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = request_SPC$Cible), color = "red") +
+    ggplot2::geom_histogram(aes(x = request_SPC$median, fill = request_SPC$median > UCL | request_SPC$median < LCL), bins = 100) +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = mean(request_SPC$median)), color = "black", linetype = 3) +
+    geom_text(aes(x = mean(request_SPC$median), label="Process Median", y=-0.5), colour="black") +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = UCL), color = "black", linetype = 3) +
+    geom_text(aes(x = UCL, label = "UCL", y = -0.5), colour = "black") +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = LCL), color = "black", linetype = 3) +
+    geom_text(aes(x = LCL, label = "LCL", y = -0.5), colour = "black") +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = request_SPC$Cible), color = "blue") +
+    geom_text(aes(x = request_SPC$Cible, label="Cible", y = 23), colour="blue") +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = USL), color = "blue") +
+    geom_text(aes(x = USL, label = "USL", y = 23), colour = "blue") +
+    ggplot2::geom_vline(ggplot2::aes(xintercept = LSL), color = "blue") +
+    geom_text(aes(x = LSL, label = "LSL", y = 23), colour = "blue") +
     stat_function(fun = dnorm, n = 101,
                   args = list(mean = mean(request_SPC$median),
                                                     sd = median(request_SPC$sd))) +
     ylab("") +
-    xlab("")
+    xlab("") + theme(legend.position = "none") + 
+    scale_fill_manual(values = c("black", "red")) 
 
-#if (Cpk < 1) {
-#  caption1 <- "Process variation is not equal to the specs"
-#}
-#  
-#if (Cpk >= 1 && Cpk < 1.3) {
-#  caption1 <- "Process variation is about equal to the specs"
-#}
-#  
-#if (Cpk >= 1.3) {
-#  caption1 <- "Process variation is equal to the specs"
-#  }
-#  
-#if (Cpk < Cp) {
-#  caption2 <- "Process is off center"
-#}
+if (Cpk < 1) {
+  caption1 <- "Process variation is not equal to the specs"
+}
+  
+if (Cpk >= 1 && Cpk < 1.3) {
+  caption1 <- "Process variation is about equal to the specs"
+}
+  
+if (Cpk >= 1.3) {
+  caption1 <- "Process variation is equal to the specs"
+  }
+  
+if (Cpk < Cp) {
+  caption2 <- "Process is off center"
+}
 
-  print(graph + labs(caption = paste(
-    paste("USL = ",USL,"\t",
-          "LSL = ", LSL,"\t", "\n",
-          "Cp = ", Cp,"\t",
-          "Cpk = ", Cpk,"\t", "\n",
-    caption1,"\n", caption2, "\n")) +
-      theme(plot.caption = element_text(hjust = 0, face = "italic"),
-            plot.title.position = "plot", 
-            plot.caption.position =  "plot")))
+
+print(graph + labs(caption = paste(
+        round(out_control_perc, 3),"% of observations are considered as outliers and are represented by the red stripes." , "\n",
+        "Cp = ", Cp,"\t",
+        "Cpk = ", Cpk,"\t", "\n",
+        caption1,"\n", caption2, "\n")) +
+    theme(plot.caption = element_text(hjust = 0, face = "italic"),
+          plot.title.position = "plot", 
+          plot.caption.position =  "plot"))
+
 }
 
 request_CL(929)
@@ -86,7 +100,6 @@ request_CL(820)
 
 # next : 
 
-# remove TRUE/FALSE Legend
 # add basic stats
 # understand how implement SL -> if we manage to do it : cpk analyis + text
 # improve presentation
