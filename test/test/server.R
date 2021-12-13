203345e3aead7af723aa70917ab31a52368b0ffc
louisdelperugia <louis.delperugia@unil.ch>
2021-12-13 17:57:19 +0100

    Times series filtering

--- test/test/server.R
+++ test/test/server.R
@@ -84,42 +84,18 @@ shinyServer(function(input, output, session) {
         )
         
     })
-    output$plot_sample <- renderPlot({
-        ggplot2::ggplot() +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids1-input$Tare), x = 1)) +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids2-input$Tare), x = 2)) +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids3-input$Tare), x = 3)) +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids4-input$Tare), x = 4)) +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids5-input$Tare), x = 5)) +
-            ggplot2::geom_point(ggplot2::aes(y = (input$Poids6-input$Tare), x = 6)) +
-            ggplot2::geom_hline(ggplot2::aes(
-                yintercept = (
-                    (input$Poids1-input$Tare + input$Poids2-input$Tare + input$Poids3-input$Tare + input$Poids4-input$Tare + input$Poids5-input$Tare + input$Poids6-input$Tare)
-                )/6
-            ),
-            color = "blue",
-            linetype = 3) +
-            ggplot2::geom_hline(yintercept = (input$Cible),
-                                linetype = "dashed",
-                                color = "red") +
-            labs(x = "Poids", y = "Weight (Gr)",
-                 title = paste("Request",input$Request),
-                 subtitle = paste("Prélèvement",input$Prelevement))
-    })
     
-    output$table_summary <- renderTable({
-        inputs <- c(input$Poids1-input$Tare, input$Poids2-input$Tare, input$Poids3-input$Tare, input$Poids4-input$Tare, input$Poids5-input$Tare, input$Poids6-input$Tare)
-        mean <- mean(inputs)
-        median <- median(inputs)
-        q1 <- quantile(inputs, probs=0.25)
-        q3 <- quantile(inputs, probs=0.75)
-        sd <- sd(inputs)
+    output$select_prelev <- renderUI({
         
-        summary <- dplyr::as_tibble(c(mean, median, q1, q3, sd))
-        summary$name <- c("mean", "median", "q1", "q3", "sd")
-        summary
-        })
-    
+        selectInput("prelevement", label = "Prelevement",
+                    
+                    
+                    choices = poids %>% 
+                        filter(Request == input$prequest) %>% 
+                        pull(Prelevement) %>% unique()
+        )
+        
+    })
     
     output$plot_hist <- renderPlot({
         request_CL(input$req)
@@ -127,6 +103,10 @@ shinyServer(function(input, output, session) {
     
     output$cible_hist <- renderPlot({
         cible_CL(input$psize, input$cible)
+    })
+    
+    output$TS_g <- renderPlot({
+        request_TS(input$prequest, input$prelevement)
     })
     
     output$summ_req <- DT::renderDataTable({
