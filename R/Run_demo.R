764a850f4705bef64ca26cb13689288054ca9c3f
dszenes <daniel.szenes@unil.ch>
2021-12-19 15:13:18 +0100

    Roxygen documentation for nasty data and Run_demo function

--- R/Run_demo.R
+++ R/Run_demo.R
@@ -1,8 +1,15 @@
-#' @title Run Shiny app for estimate_area function
+#' @title Run Shiny app for pkgproject
 #'
 #' @description Runs an interactive Shiny app that allows the user
-#'  use the functions from the pkgproject package
+#'  to organise inputs and analysis for his process data
 #' @author Özgür Aydemir, Sophie La Gennusa, Louis del Perugia, Daniel Szenes, Francesca Darino
+#' @return A \code{Shiny app} containing the following elements:
+#'  \describe{
+#'      \item{Input tab}{Tab where one can input new values and see historical data}
+#'      \item{Density plot}{Tab that displays analytical plots and summary statistics per request}
+#'      \item{Time seires tab}{Tab where one can see the values for measurements per request per sample}
+#'      \item{App info tab}{Tab with general information about the app}
+#' }
 #' @export
 Run_demo <- function() {
   appDir <- system.file( "test", package = "pkgproject")
---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Example data for pkgproject package
+#'
+#' A dataset containing simulated measurements of a business process
+#' with other attributes.
+#'
+#' @format A data frame with 970 rows and 13 variables:
+#' \describe{
+#'   \item{Date}{Date of sample}
+#'   \item{Operator}{Name of operator}
+#'   \item{Process.Sample}{Sample number within the request}
+#'   \item{Product size}{Size of the measured product}
+#'   \item{Measure 1 to 6}{6 columns for 6 measurments taken within one sample}
+#'   \item{Target.Value}{Value of reference for measured objects}
+#'   \item{Tare}{Weight of cantainer}
+#'   \item{Request}{Request number}
+#' }
+"nasty"
